5c6f43d9af217330867e926b74f0914db41dc69c
Taka-Adelaide <takapenang08@gmail.com>
2021-12-08 10:09:54 +0000

    会員登録　ランク登録修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   attachment :image
-  enum rank: { non_diver: 0, OW: 1, AOW: 2, RD: 3, MSD:4, DM:5, IA:6}
+  enum rank: { non_diver: 0, OW: 1, AOW: 2, RD: 3, MSD: 4, DM: 5, IA: 6}
 end
