a70421f9465ddeb88c80694d3a67f7dbc644941c
Taka-Adelaide <takapenang08@gmail.com>
2021-12-09 11:36:26 +0000

    エリア作成

--- app/admin/areas.rb
+++ app/admin/areas.rb
@@ -1,18 +1,19 @@
-ActiveAdmin.register Spot do
+ActiveAdmin.register Area do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :area_id, :name, :caption, :has_beach, :has_boat, :image_id, :onsen, :is_active
+  # permit_params :name
   #
   # or
   #
   # permit_params do
-  #   permitted = [:area_id, :name, :has_beach, :has_boat, :image_id, :onsen, :is_active]
+  #   permitted = [:name]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+  permit_params :name
 
 end
