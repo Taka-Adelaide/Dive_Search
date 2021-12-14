085cc6fa526c2cadef5d7417850b39cd635a3d47
Taka-Adelaide <takapenang08@gmail.com>
2021-12-14 04:49:54 +0000

    spot_favorite非同期通信エラー

--- app/controllers/spot_favorites_controller.rb
+++ app/controllers/spot_favorites_controller.rb
@@ -3,14 +3,14 @@ class SpotFavoritesController < ApplicationController
     spot = Spot.find(params[:spot_id])
     favorite = current_user.spot_favorites.new(user_id: current_user.id, spot_id: spot.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
     spot_id = Spot.find(params[:spot_id])
     favorite = current_user.spot_favorites.find_by(spot_id: spot_id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 end
