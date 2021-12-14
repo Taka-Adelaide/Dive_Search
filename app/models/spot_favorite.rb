73deb5fded090f9cb7aefd060d216e51b4dcfef0
Taka-Adelaide <takapenang08@gmail.com>
2021-12-14 11:03:01 +0000

    コメントいいね実装

--- app/models/spot_favorite.rb
+++ app/models/spot_favorite.rb
@@ -2,7 +2,7 @@ class SpotFavorite < ApplicationRecord
   belongs_to :user
   belongs_to :spot
   
-  def favorited_by?(current_user)
-    spot_favorites.where(user_id: current_user.id).exists?
-  end
+  # def favorited_by?(current_user)
+  #   spot_favorites.where(user_id: current_user.id).exists?
+  # end
 end
