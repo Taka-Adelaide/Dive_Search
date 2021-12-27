07283bbbe8d8b0a73b9ac4679d137528567377e9
Taka-Adelaide <takapenang08@gmail.com>
2021-12-27 05:29:46 +0000

    env

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # AdminUser.create!(email: ENV['admin_email'], password: ENV['admin_password'], password_confirmation: ENV['admin_password'])if Rails.env.development?
-AdminUser.create!(email: 'admin@example.com', password: ENV['admin_password'], password_confirmation: ENV['admin_password'])if Rails.env.development?
+AdminUser.create!(email: ENV['admin_email'], password: ENV['admin_password'], password_confirmation: ENV['admin_password'])if Rails.env.development?
